dd5db770ec4b09818293e2fe8e214d1e8fc5b73f
Muhammad-Jafri <24100113@lums.edu.pk>
2024-06-17 02:30:02 +0500

    implement boilerplate

---
+++ Graph.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+
+class Graph:
+
+    def __init__(self):
+
+        self.adj_list_rep = {}
+
+
+    def bfs(self, src):
+
+        raise NotImplementedError
+    
+
+    def dfs(self, src):
+
+        raise NotImplementedError
